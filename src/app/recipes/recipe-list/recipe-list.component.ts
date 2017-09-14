4c9ed0153fbcc2a18ab8784f5ba2ac1fe542fc33
Chisomo Lungu <chisomo@gmail.com>
2017-09-14 06:21:54 +0100

    added nested components and using bootstrap for styling

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://www.nps.gov/subjects/camping/images/recipe_1.jpg'),
+    new Recipe('A Test Recipe', 'This is a test', 'https://www.nps.gov/subjects/camping/images/recipe_1.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
